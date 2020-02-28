ba4f48e9799cb3d7688f523369ce75be39c2c840
viccom <3520845@qq.com>
2020-02-28 20:01:50 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,1 +1,20 @@
+<?php
 
+// echo PHP_SAPI;
+
+exec("netstat -lntp|grep 0.0.0.0:1688 2>&1",$output,$ret_val);
+
+
+if($ret_val == 0){
+    
+    // $scope =  implode('|',$output);
+    // echo $scope;
+    echo "online";
+    
+}
+else{
+    
+    echo "offline";
+}
+exit;
+?>
